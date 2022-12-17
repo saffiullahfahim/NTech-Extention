32c69fde1e6125908c7c238eaab5cb3d2af74190
saffiullahfahim <fahim14@student.sust.edu>
2022-12-17 08:23:48 +0600

    present version

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-const foo = "This is foo";
+console.log("test");
 
-export {foo};
+window.addEventListener("load", () => {
+  const ntechIcon = document.createElement("div");
+  ntechIcon.setAttribute("ntech", "iconDiv");
+  ntechIcon.innerHTML = "<img ntech='icon' src='https://ntechcomputer.github.io/icon.png' alt='NTech Icon'>"
+  document.body.appendChild(ntechIcon);
+});
